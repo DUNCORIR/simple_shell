368dd3235791e3556b423389b9a529e76aa579bd
duncorir <duncorir@mail.com>
2024-08-28 15:40:53 +0300

    Shell Project

--- command_separator.c
+++ command_separator.c
@@ -13,7 +13,6 @@ void execute_commands(char *input, char **environ, char *program_name, int line_
 	int last_status = 0;
 	char *input_copy = custom_strdup(input);
 	char *delim = ";";
-	char **parsed_args = NULL;
 
 	if (input_copy == NULL)
 	{
@@ -25,36 +24,36 @@ void execute_commands(char *input, char **environ, char *program_name, int line_
 	while (command != NULL)
 	{
 		char *trimmed_command = custom_strtok(command, "\n");
-		int should_execute = 1;
 
 		if (trimmed_command != NULL && *trimmed_command != '\0')
 		{
 			/* Check for '&&' and '||' at the beginning of the command */
-			if (strncmp(trimmed_command, "&&", 2) == 0)
+			char *subcommand;
+
+			subcommand = custom_strtok(trimmed_command, "&&"); /* Handle '&&' */
+			while (subcommand != NULL)
 			{
-				if (last_status != 0)
-					should_execute = 0; /* Skip if the last command failed */
-				trimmed_command += 2;  /* Move past '&&' */
-			}
-			else if (strncmp(trimmed_command, "||", 2) == 0)
-			{
-				if (last_status == 0)
-					should_execute = 0; /* Skip if the last command succeeded */
-				trimmed_command += 2;  /* Move past '||' */
-			}
-			if (should_execute && *trimmed_command != '\0')
-			{
-				char **parsed_args = parse_input(trimmed_command);
-				if (parsed_args[0] != NULL)
+				char *subsubcommand = custom_strtok(subcommand, "||"); /* Handle '||' */
+				while (subsubcommand != NULL)
 				{
-					int builtin_status = handle_builtins(parsed_args, environ, program_name, line_number);
-					if (!builtin_status)
-						last_status = execute_command(parsed_args, environ, program_name, line_number);
+					char **parsed_args = parse_input(subsubcommand);
+
+					if (parsed_args[0] != NULL)
+					{
+						if (last_status == 0)
+						{
+
+							last_status = execute_command(parsed_args, environ, program_name, line_number);
+						}
+					}
+					free(parsed_args); /* Free the argument list */
+					subsubcommand = custom_strtok(NULL, "||");
 				}
+				subcommand = custom_strtok(NULL, "&&");
 			}
-			free(parsed_args); /* Free the argument list */
 		}
 		command = custom_strtok(NULL, delim); /* Get the next command */
 	}
+
 	free(input_copy); /* Free duplicated input */
 }
