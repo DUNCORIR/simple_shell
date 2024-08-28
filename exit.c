b310fe65e54c2ef07251887f7f2ad102902b7d91
duncorir <duncorir@mail.com>
2024-08-28 15:55:36 +0300

    exit

--- exit.c
+++ exit.c
@@ -10,23 +10,7 @@
  * it exits with the status of the last
  *
  */
-void execute_exit(char **args)
+void execute_exit( __attribute__((unused)) char **args)
 {
-	/* Default exit status if no argument is provided */
-	int status = 0;
-
-	if (args[1] != NULL)
-	{
-		/* Convert the argument to an integer status code */
-		char *endptr; 
-		status = strtol(args[1], &endptr, 10);
-
-		if (*endptr != '\0')
-		{
-			fprintf(stderr, "Invalid exit status: %s\n", args[1]);
-			return; /* Return without exiting if status is invalid */
-		}
-	}
-	/*  Exit the shell with the provided or default status */
-	exit(status);
+	exit(EXIT_SUCCESS);
 }
